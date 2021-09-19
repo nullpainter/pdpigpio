77a89291b2a1eb2e0320209d88c0ed5451d60b7e
Matt Painter <matt@deity.co.nz>
2021-09-19 02:14:47 +0100

    Fixed use of globals

--- pigpio_mcp3008.c
+++ pigpio_mcp3008.c
@@ -6,16 +6,6 @@
 #include <pigpio.h>
 
 static t_class *pigpio_mcp3008_class;
-static bool initialised = false;
-
-static unsigned char buffer[3];
-static char command[3];
-
-#define BAUD 250000
-#define FLAGS 0 
-
-// Number of MCP3008 pins we're reading
-#define INPUT_PINS 8
 
 typedef struct _pigpio_mcp3008
 {
@@ -26,62 +16,70 @@ typedef struct _pigpio_mcp3008
     int x_mosi_gpio;
     int x_sclk_gpio;
 
+    unsigned char buffer[3];
+    char command[3];
+
+    int baud;
+
+    // SPI flags
+    int flags;
+
+    // Number of MCP3008 pins we're reading
+    int input_pins;
+
+    bool initialised;
+
 } t_pigpio_mcp3008;
 
 static void pigpio_mcp3008_free(t_pigpio_mcp3008 *x)
 {
-    // Close SPI connection
-    bbSPIClose(x->x_cs_gpio);
-}
-
-static void pigpio_mcp3008_initialise(t_pigpio_mcp3008 *x)
-{
-    if (initialised)
+    // Close SPI connection if required
+    if (x->initialised)
     {
-        return;
+        bbSPIClose(x->x_cs_gpio);
+        post("[pigpio_mcp3008] closing for cs:%d", x->x_cs_gpio);
     }
 
-    // Open SPI connection
-    bbSPIOpen(x->x_cs_gpio, x->x_miso_gpio, x->x_mosi_gpio, x->x_sclk_gpio, BAUD, FLAGS);
-    initialised = true;
+    outlet_free(x->x_out);
+    gpioTerminate();
+
+    x->initialised = false;
 }
 
 static void pigpio_mcp3008_bang(t_pigpio_mcp3008 *x)
 {
     // Initialise SPI on first bang
-    if (!initialised)
+    if (!x->initialised)
     {
-        pigpio_mcp3008_initialise(x);
+        bbSPIOpen(x->x_cs_gpio, x->x_miso_gpio, x->x_mosi_gpio, x->x_sclk_gpio, x->baud, x->flags);
+        x->initialised = true;
+
+        post("[pigpio_mcp3008] initialised for cs:%d", x->x_cs_gpio);
     }
 
-    command[0] = 1;
-    command[2] = 0;
+    x->command[0] = 1;
+    x->command[2] = 0;
 
-    t_atom values[INPUT_PINS];
+    t_atom values[x->input_pins];
     float value;
 
     // Read from all MCP3008 input pins
-    for (int i = 0; i < INPUT_PINS; i++) 
+    for (int i = 0; i < x->input_pins; i++)
     {
-        command[1] = (i + 8) << 4;
-        bbSPIXfer(x->x_cs_gpio, command, (char *)buffer, 3);
+        x->command[1] = (i + 8) << 4;
+        bbSPIXfer(x->x_cs_gpio, x->command, (char *)x->buffer, 3);
 
         // Extract value, normalise to 0-1
-        value = (((buffer[1] & 3) << 8) | buffer[2]) / 1023.0f;
+        value = (((x->buffer[1] & 3) << 8) | x->buffer[2]) / 1023.0f;
         SETFLOAT(&values[i], value);
     }
 
     // Output all values as a list
-    outlet_list(x->x_out, 0, INPUT_PINS, values);
+    outlet_list(x->x_out, 0, x->input_pins, values);
 }
 
 static void *pigpio_mcp3008_new(t_floatarg f1, t_floatarg f2, t_floatarg f3, t_floatarg f4)
 {
-    if (initialised) 
-    {
-       initialised = false;
-    }
-
     t_pigpio_mcp3008 *x = (t_pigpio_mcp3008 *)pd_new(pigpio_mcp3008_class);
 
     // Assign arguments
@@ -90,23 +88,15 @@ static void *pigpio_mcp3008_new(t_floatarg f1, t_floatarg f2, t_floatarg f3, t_f
     x->x_mosi_gpio = f3;
     x->x_sclk_gpio = f4;
 
-    post("[pigpio_mcp3008] cs:%d, miso:%d, mosi:%d, sclk:%d", x->x_cs_gpio, x->x_miso_gpio, x->x_mosi_gpio, x->x_sclk_gpio);
-
-    if (geteuid() != 0)
-    {
-        pd_error(x, "[pigpio_mcp3008] root is required to use this external");
-        exit(0);
-    }
-
-    // Use PWM for timing DMA transfers, as PCM is used for audio
-    gpioCfgClock(5, 0, 0);
-    if (gpioInitialise() < 0)
-    {
-        pd_error(x, "[pigpio_mcp3008] Unable to initialise GPIO; ensure pigpiod isn't running");
-        exit(0);
-    }
+    // Set constants
+    x->baud = 50000;
+    x->flags = 0;
+    x->input_pins = 8;
 
     x->x_out = outlet_new(&x->x_obj, gensym("list"));
+    x->initialised = false;
+
+    post("[pigpio_mcp3008] cs:%d, miso:%d, mosi:%d, sclk:%d", x->x_cs_gpio, x->x_miso_gpio, x->x_mosi_gpio, x->x_sclk_gpio);
 
     return x;
 }
